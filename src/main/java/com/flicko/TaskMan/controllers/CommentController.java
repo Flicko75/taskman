5a414c55b62d40e7b6183bab2adb177e2f576d66
Flicko75 <142166063+Flicko75@users.noreply.github.com>
2026-02-27 20:48:13 +0530

    Role based enforcement of comment service methods

--- src/main/java/com/flicko/TaskMan/controllers/CommentController.java
+++ src/main/java/com/flicko/TaskMan/controllers/CommentController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.nio.file.AccessDeniedException;
 import java.util.List;
 
 @RestController
@@ -23,31 +24,31 @@ public class CommentController {
 
     @GetMapping("/tasks/{taskId}/comments")
     @PreAuthorize("hasAnyRole('ADMIN','MANAGER','MEMBER')")
-    public PageResponse<CommentResponse> getAllComments(@PathVariable Long taskId, Pageable pageable){
+    public PageResponse<CommentResponse> getAllComments(@PathVariable Long taskId, Pageable pageable) throws AccessDeniedException {
         return commentService.getAllComments(taskId, pageable);
     }
 
     @GetMapping("comments/{commentId}")
     @PreAuthorize("hasAnyRole('ADMIN','MANAGER','MEMBER')")
-    public CommentResponse getComment(@PathVariable Long commentId){
+    public CommentResponse getComment(@PathVariable Long commentId) throws AccessDeniedException {
         return commentService.getCommentById(commentId);
     }
 
     @PostMapping("/tasks/{taskId}/comments")
     @PreAuthorize("hasAnyRole('ADMIN','MANAGER','MEMBER')")
-    public CommentResponse addComment(@PathVariable Long taskId, @Valid @RequestBody CommentCreate comment){
+    public CommentResponse addComment(@PathVariable Long taskId, @Valid @RequestBody CommentCreate comment) throws AccessDeniedException {
         return commentService.addComment(taskId, comment);
     }
 
     @PutMapping("/comments/{commentId}")
     @PreAuthorize("hasAnyRole('ADMIN','MANAGER','MEMBER')")
-    public CommentResponse updateComment(@PathVariable Long commentId, @Valid @RequestBody CommentUpdate comment){
+    public CommentResponse updateComment(@PathVariable Long commentId, @Valid @RequestBody CommentUpdate comment) throws AccessDeniedException {
         return commentService.updateComment(commentId, comment);
     }
 
     @DeleteMapping("/comments/{commentId}")
     @PreAuthorize("hasAnyRole('ADMIN','MANAGER','MEMBER')")
-    public void deleteComment(@PathVariable Long commentId){
+    public void deleteComment(@PathVariable Long commentId) throws AccessDeniedException {
         commentService.deleteComment(commentId);
     }
 
--- src/main/java/com/flicko/TaskMan/services/CommentService.java
+++ src/main/java/com/flicko/TaskMan/services/CommentService.java
@@ -4,6 +4,7 @@ import com.flicko.TaskMan.DTOs.CommentCreate;
 import com.flicko.TaskMan.DTOs.CommentResponse;
 import com.flicko.TaskMan.DTOs.CommentUpdate;
 import com.flicko.TaskMan.DTOs.PageResponse;
+import com.flicko.TaskMan.enums.UserRole;
 import com.flicko.TaskMan.exceptions.InvalidOperationException;
 import com.flicko.TaskMan.exceptions.ResourceNotFoundException;
 import com.flicko.TaskMan.models.Comment;
@@ -13,12 +14,17 @@ import com.flicko.TaskMan.repos.CommentRepository;
 import com.flicko.TaskMan.repos.TaskRepository;
 import com.flicko.TaskMan.repos.UserRepository;
 import com.flicko.TaskMan.utils.PageMapper;
+import com.flicko.TaskMan.utils.SecurityUtils;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.nio.file.AccessDeniedException;
 
 @Service
 @RequiredArgsConstructor
@@ -30,40 +36,85 @@ public class CommentService {
 
     private final UserRepository userRepository;
 
-    public PageResponse<CommentResponse> getAllComments(Long taskId, Pageable pageable) {
-        taskRepository.findById(taskId)
+    private final SecurityUtils securityUtils;
+
+    public PageResponse<CommentResponse> getAllComments(Long taskId, Pageable pageable) throws AccessDeniedException {
+        User user = securityUtils.getCurrentUser();
+
+        Task task = taskRepository.findById(taskId)
                 .orElseThrow(() -> new ResourceNotFoundException("Task not found"));
 
-        Page<Comment> comments = commentRepository.findByTaskIdOrderByCreatedAtAsc(taskId, pageable);
+        Page<Comment> comments;
+        if (user.getRole() == UserRole.ADMIN){
+            comments = commentRepository.findByTaskIdOrderByCreatedAtAsc(taskId, pageable);
+        } else if (user.getRole() == UserRole.MANAGER) {
+            if (user.getTeam() == null ||
+                task.getTeam() == null ||
+                !task.getTeam().getId().equals(user.getTeam().getId()))
+                throw new AccessDeniedException("Can't fetch comments of other teams");
+            comments = commentRepository.findByTaskIdOrderByCreatedAtAsc(taskId, pageable);
+        } else if (user.getRole() == UserRole.MEMBER) {
+            if (task.getUser() == null ||
+                !task.getUser().getId().equals(user.getId()))
+                throw new AccessDeniedException("Can't fetch comments of unassigned tasks");
+            comments = commentRepository.findByTaskIdOrderByCreatedAtAsc(taskId, pageable);
+        }else
+            throw new AccessDeniedException("Access Denied");
 
         Page<CommentResponse> mapped = comments.map(this::mapToResponse);
 
         return PageMapper.toPageResponse(mapped);
     }
 
-    public CommentResponse getCommentById(Long commentId) {
+    public CommentResponse getCommentById(Long commentId) throws AccessDeniedException {
+        User user = securityUtils.getCurrentUser();
+
         Comment comment = commentRepository.findById(commentId)
                 .orElseThrow(() -> new ResourceNotFoundException("Comment not found"));
+
+        Task task = comment.getTask();
+
+        if (user.getRole() == UserRole.ADMIN){
+
+        } else if (user.getRole() == UserRole.MANAGER) {
+            if (user.getTeam() == null ||
+                task.getTeam() == null ||
+                !task.getTeam().getId().equals(user.getTeam().getId()))
+                throw new AccessDeniedException("Can't fetch comments of other teams");
+        } else if (user.getRole() == UserRole.MEMBER) {
+            if (task.getUser() == null ||
+                !task.getUser().getId().equals(user.getId()))
+                throw new AccessDeniedException("Can't fetch comments of unassigned tasks");
+        } else
+            throw new AccessDeniedException("Access Denied");
 
         return mapToResponse(comment);
     }
 
     @Transactional
-    public CommentResponse addComment(Long taskId, @Valid CommentCreate comment) {
+    public CommentResponse addComment(Long taskId, @Valid CommentCreate comment) throws AccessDeniedException {
+        User currentUser = securityUtils.getCurrentUser();
+
         Task task = taskRepository.findById(taskId)
                 .orElseThrow(() -> new ResourceNotFoundException("Task not found"));
 
-        User user = userRepository.findById(comment.getUserId())
-                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+        if (currentUser.getRole() == UserRole.ADMIN){
 
-        if (user.getTeam() == null
-                || task.getTeam() == null
-                || !user.getTeam().getId().equals(task.getTeam().getId()))
-            throw new InvalidOperationException("User and Task needs to belong to same team");
+        } else if (currentUser.getRole() == UserRole.MANAGER) {
+            if (currentUser.getTeam() == null ||
+                task.getTeam() == null ||
+                !task.getTeam().getId().equals(currentUser.getTeam().getId()))
+                throw new AccessDeniedException("Can't add comment to other team tasks");
+        } else if (currentUser.getRole() == UserRole.MEMBER) {
+            if (task.getUser() == null ||
+                !task.getUser().getId().equals(currentUser.getId()))
+                throw new AccessDeniedException("Can't add comment to unassigned tasks");
+        }else
+            throw new AccessDeniedException("Access Denied");
 
         Comment comment1 = new Comment();
 
-        comment1.setUser(user);
+        comment1.setUser(currentUser);
         comment1.setTask(task);
         comment1.setContent(comment.getContent());
 
@@ -71,9 +122,27 @@ public class CommentService {
     }
 
     @Transactional
-    public CommentResponse updateComment(Long commentId, @Valid CommentUpdate comment) {
+    public CommentResponse updateComment(Long commentId, @Valid CommentUpdate comment) throws AccessDeniedException {
+        User user = securityUtils.getCurrentUser();
+
         Comment oldcomment = commentRepository.findById(commentId)
                 .orElseThrow(() -> new ResourceNotFoundException("Comment not found"));
+
+        Task task = oldcomment.getTask();
+
+        if (user.getRole() == UserRole.ADMIN){
+
+        } else if (user.getRole() == UserRole.MANAGER) {
+            if (user.getTeam() == null ||
+                task.getTeam() == null ||
+                !task.getTeam().getId().equals(user.getTeam().getId()))
+                throw new AccessDeniedException("Can't update comments of other teams");
+        } else if (user.getRole() == UserRole.MEMBER) {
+            if (oldcomment.getUser() == null ||
+                !oldcomment.getUser().getId().equals(user.getId()))
+                throw new AccessDeniedException("Can't update comments of other members");
+        } else
+            throw new AccessDeniedException("Access Denied");
 
         oldcomment.setContent(comment.getContent());
 
@@ -81,9 +150,27 @@ public class CommentService {
     }
 
     @Transactional
-    public void deleteComment(Long commentId) {
+    public void deleteComment(Long commentId) throws AccessDeniedException {
+        User user = securityUtils.getCurrentUser();
+
         Comment comment = commentRepository.findById(commentId)
                 .orElseThrow(() -> new ResourceNotFoundException("Comment not found"));
+
+        Task task = comment.getTask();
+
+        if (user.getRole() == UserRole.ADMIN){
+
+        } else if (user.getRole() == UserRole.MANAGER) {
+            if (user.getTeam() == null ||
+                task.getTeam() == null ||
+                !task.getTeam().getId().equals(user.getTeam().getId()))
+                throw new AccessDeniedException("Can't delete comments of other teams");
+        } else if (user.getRole() == UserRole.MEMBER) {
+            if (comment.getUser() == null ||
+                !comment.getUser().getId().equals(user.getId()))
+                throw new AccessDeniedException("Can't delete comments of other members");
+        } else
+            throw new AccessDeniedException("Access Denied");
 
         commentRepository.delete(comment);
     }
